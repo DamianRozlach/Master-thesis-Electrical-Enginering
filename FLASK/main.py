06e0352a3c01bf8366e9c98c9cd54a9f58bdb6f3
Damian Rozlach <damianrozlach@gmail.com>
2022-04-11 01:00:15 +0200

    0.01 sleep

--- FLASK/main.py
+++ FLASK/main.py
@@ -41,7 +41,7 @@ def index():
 def gen(camera):
     #get camera frame
     while True:
-        eventlet.sleep(0.5)
+        eventlet.sleep(0.01)
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
