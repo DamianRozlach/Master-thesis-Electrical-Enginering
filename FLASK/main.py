eb0d6a05d01a6707fb13a5323a70240178adef87
Damian Rozlach <damianrozlach@gmail.com>
2022-04-10 21:04:55 +0200

    communication

--- FLASK/main.py
+++ FLASK/main.py
@@ -49,32 +49,29 @@ def control(message):
         print("received a steering mesage")
         tank.x_axis = data["steeringData"]["X"]
         tank.y_axis = data["steeringData"]["Y"]
-        print(tank.x_axis)
-        print(tank.y_axis)
         if isDev: print(tank)
         #linear.q.put(("left",x,y))
     elif "On" in data.keys():
         print("received a on message")
-        x = data["right"][0]
-        y = data["right"][1]
-        if TRUE: print("[Server] Right: ",x,",",y)
-        #servo.q.put(("right",x,y))
-        #servo2.q.put(("right",y,x))
-    elif "A" in data.keys():
-        if TRUE: print("[Server] A")
-        #binary.q.put(("A",1,0))
-    elif "B" in data.keys():
-        if True: print("[Server] B")
-        #binary2.q.put(("B",1,0))
+    elif "Off" in data.keys():
+        print("received a off message")
+    elif "Up" in data.keys():
+        print("received a up message")
+    elif "Down" in data.keys():
+        print("received a down message")
+    elif "Left" in data.keys():
+        print("received a left message")
+    elif "Right" in data.keys():
+        print("received a right message")
+
 
 
 @socketio.on_error_default
 def default_error_handler(e):
-    pass
-    #print("======================= ERROR =======================")
-    #print(request.event["message"])
-    #print(request.event["args"])
-    #print(e)
+    print("======================= ERROR =======================")
+    print(request.event["message"])
+    print(request.event["args"])
+    print(e)
 
 if __name__ == '__main__':
 
