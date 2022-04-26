8f177dba2d7c39572c14f8c7a63cc6b9f0f09a7c
Damian Rozlach <damianrozlach@gmail.com>
2022-04-26 23:19:50 +0200

    written some tasks for atmega

--- FreeRtosCodes/atmega_328p_code/atmega_328p_code.ino
+++ FreeRtosCodes/atmega_328p_code/atmega_328p_code.ino
@@ -2,17 +2,36 @@
 #include <semphr.h>
 #include <Wire.h>
 
-#define slaveAdressI2c 8
-#define M1A 1
-#define M1B 2
-#define M2A 3
-#define M2B 4
 #define debug 1
 
+#define slaveAdressI2c 8
+#define M1A 6
+#define M1B 9
+#define M2A 10
+#define M2B 11
+#define servo1Pin 3
+#define servo2Pin 5
+#define leftSensor A0
+#define rightSensor A1
+
+#define maxSpeed 255
+#define servo1Min 0
+#define servo1Max 255
+#define servo2Min 0
+#define servo2Max 255
+
 TaskHandle_t taskCommunicationHandler;
+TaskHandle_t TaskAutonomousControlHandler;
+TaskHandle_t TaskRemoteControlHandler;
+TaskHandle_t TaskRangeSensorHandler;
+TaskHandle_t TaskLineSensorsHandler;
 
 SemaphoreHandle_t xDataSemaphore;
-SemaphoreHandle_t interruptSemaphore;
+SemaphoreHandle_t autonomousSemaphore;
+
+const int motorPins[] = {M1A,M1B,M2A,M2B};
+int sizeArr = sizeof(motorPins) / sizeof(int);
+
 
 bool mode = false;
 int LeftTrack = 0;
@@ -30,33 +49,66 @@ void TaskLineSensors(void *pvParameters);
 
 void setup() 
 {
+  initializeHardware();
+  
   xTaskCreate(TaskCommunication, // Task function
               "I2C_communication", // Task name
               128, // Stack size
               NULL,
-              3, // Priority
+              2, // Priority
               &taskCommunicationHandler ); // TaskHandle
+
+  xTaskCreate(TaskAutonomousControl, // Task function
+              "Autonomous Control", // Task name
+              128, // Stack size
+              NULL,
+              3, // Priority
+              &TaskAutonomousControlHandler ); // TaskHandle
+
+  xTaskCreate(TaskRemoteControl, // Task function
+              "TaskRemoteControl", // Task name
+              128, // Stack size
+              NULL,
+              0, // Priority
+              &TaskRemoteControlHandler ); // TaskHandle
+
+  xTaskCreate(TaskRangeSensor, // Task function
+              "TaskRangeSensor", // Task name
+              128, // Stack size
+              NULL,
+              1, // Priority
+              &TaskRangeSensorHandler ); // TaskHandle
+
+  xTaskCreate(TaskLineSensors, // Task function
+              "TaskLineSensors", // Task name
+              128, // Stack size
+              NULL,
+              1, // Priority
+              &TaskLineSensorsHandler ); // TaskHandle
+
+   vTaskSuspend(TaskAutonomousControlHandler);
 }
 
 void loop() {
   // not used
 }
 
-// ----------------------------------------- TASKS ----------------------------
+// ----------------------------------------- TASK Communication ----------------------------
 
 void TaskCommunication(void *pvParameters)
 {
    (void) pvParameters;
+   bool lastState = false;
 
    Wire.setClock(100000);
-   Wire.begin(slaveAdressI2c);                // join i2c bus
+   Wire.begin(slaveAdressI2c);                
    Wire.onReceive(receiveEventHandler);
    if(debug)
    {
       Serial.begin(9600);
       while (!Serial) 
       {
-      ; // just wait for Serial connection
+      ; // wait for Serial connection
       }
     }
     for(;;)
@@ -73,10 +125,10 @@ void TaskCommunication(void *pvParameters)
                mode = bool(x);
              break;
              case 3:
-               LeftTrack = x;
+               LeftTrack = x - 100;
              break;
              case 2:
-               RightTrack = x;
+               RightTrack = x - 100;
              break;
              case 1:
                servo1 = x;
@@ -95,11 +147,59 @@ void TaskCommunication(void *pvParameters)
              Serial.print(" ");
            }
         }
+        if(debug){
+          Serial.println();
+        }
         Serial.println();
+        if(mode != lastState){
+          if(mode){
+            vTaskResume( TaskAutonomousControlHandler );
+          } else {
+            vTaskSuspend(TaskAutonomousControlHandler);
+          }
+        }
+        if(!mode){
+          xTaskNotifyGive(TaskRemoteControlHandler);
+        }
+        lastState = mode;
       } 
     }
    
  }
+
+// ----------------------------------------- TASK Autonomous Control ----------------------------
+void TaskAutonomousControl(void *pvParameters)
+{
+  (void) pvParameters;
+}
+
+// ----------------------------------------- TASK Remote Control ----------------------------
+void TaskRemoteControl(void *pvParameters)
+{
+  (void) pvParameters;
+
+  for(;;){
+    ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+    setMotors(LeftTrack,RightTrack);
+    analogWrite(servo1Pin,map(servo1,0,20,servo1Min,servo1Max));
+    analogWrite(servo2Pin,map(servo2,0,20,servo2Min,servo2Max));
+  } 
+}
+
+// ----------------------------------------- TASK Range Sensor ----------------------------
+void TaskRangeSensor(void *pvParameters)
+{
+  (void) pvParameters;
+}
+
+// ----------------------------------------- TASK Line Sensors ----------------------------
+void TaskLineSensors(void *pvParameters)
+{
+  (void) pvParameters;
+}
+
+// ----------------------------------------- event notification ----------------------------
+
 
 void receiveEventHandler(int howMany)
 {
@@ -109,4 +209,55 @@ void receiveEventHandler(int howMany)
   {
     taskYIELD();
   }
+}
+
+// ----------------------------------------- functions ----------------------------
+
+void setMotors(int leftMotor, int rightMotor){
+  /*
+  if(leftMotor == 0){
+    analogWrite(motorPins[2], 0);
+    analogWrite(motorPins[3], 0);
+  }
+  if(rightMotor == 0){
+    analogWrite(motorPins[0], 0);
+    analogWrite(motorPins[1], 0);
+  }
+  */
+  if(leftMotor >= 0){
+    analogWrite(motorPins[2], map(leftMotor,0,100,0,maxSpeed));
+    analogWrite(motorPins[3], 0);
+  }
+  if(leftMotor < 0){
+    analogWrite(motorPins[2], 0 );
+    analogWrite(motorPins[3], map(leftMotor,0,-100,0,maxSpeed));
+  }
+  if(rightMotor >= 0){
+    analogWrite(motorPins[0], map(rightMotor,0,100,0,maxSpeed));
+    analogWrite(motorPins[1], 0);
+  }
+  if(rightMotor < 0){
+    analogWrite(motorPins[0], 0 );
+    analogWrite(motorPins[1], map(rightMotor,0,-100,0,maxSpeed));
+  }
+}
+
+void stopMotors(){
+  for(int z = 0; z < sizeArr;z++){
+    analogWrite(motorPins[z], 0);
+  }
+}
+
+void initializeHardware(){
+  for(int z = 0; z < sizeArr ;z++){
+    pinMode(motorPins[z]  , OUTPUT);
+  }
+
+  stopMotors();
+
+  pinMode(servo1Pin,OUTPUT);
+  pinMode(servo2Pin,OUTPUT);
+
+  analogWrite(servo1Pin,map(10,0,20,servo1Min,servo1Max));
+  analogWrite(servo2Pin,map(10,0,20,servo2Min,servo2Max));
 }
